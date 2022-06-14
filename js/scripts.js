40af48a4991ec46cf00960e21011b8713da8aaa3
pj perry <pjperry28@gmail.com>
2022-06-13 17:54:37 -0700

    add js

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+// Business logic for Address book --------
+function AddressBook (){
+    this.contacts= {};
+    this.currentId = 0;
+    AddressBook.prototype.addContact = function (contact) {
+        contact.id =this.assignId();
+        this.contacts[contact.id] = contact ;
+    };
+        AddressBook.prototype.assignId = function(){
+            this.currentId +=1;
+            return this.currentId;
+        }
+    this.contacts[contact.firstName] = contact;
+};
+
+// Business logic for Contacts --------
+function Contact(firstName,lastName,phoneNumber){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.phoneNumber = phoneNumber;
+
+}
+ 
+Contact.prototype.fullName = function (){
+    return this.firstName + " " + this.lastName;
+};
